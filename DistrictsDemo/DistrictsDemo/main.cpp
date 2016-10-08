f8126e3a1c2afb0467760878afa810136a75dbf6
Alliy Frauenpreis <anf46@drexel.edu>
2016-10-07 20:22:52 -0400

    cleanup of code and addition of check for too close random points

--- DistrictsDemo/DistrictsDemo/main.cpp
+++ DistrictsDemo/DistrictsDemo/main.cpp
@@ -29,110 +29,123 @@ using namespace std;
  
  For any additional points, find the two points closet to the new point and repeat the above process.
  
+ for vectors, [i][0] is x value, [1][1] is y value
+ 
  http://stackoverflow.com/questions/2512978/how-to-delete-duplicate-vectors-within-a-multidimensional-vector
  */
 
 int main(int argc, const char * argv[]) {
     
+    int numDistricts = atoi(argv[1]);   // passed argument for number of districts; currently only support 3
+    int districtSpan = 500;             // the length of one vector that creates a district barrier.
     
-    
-    int numDistricts = 3;
-    int sceneWidth = 500;
-    int sceneHeight = 500;
-    
-    int edgeBuffer = 100; // this is the buffer of how far we want the points to at least be from the edge.
-    vector<vector<int>> startPoints(numDistricts);
-    vector<vector<int>> midPoints(numDistricts);
-    vector<vector<float>> endPoints(numDistricts);
+    vector<vector<int>> startPoints(numDistricts);  // randomly initialized district points
+    vector<vector<int>> midPoints(numDistricts);    // midpoints between all random district points
+    vector<vector<float>> endPoints(numDistricts);  // end of line segments from center point that split districts
     
     startPoints.reserve(numDistricts);
     
-    startPoints.reserve(numDistricts);
     for (int i = 0; i < numDistricts; i++){
         
         startPoints[i].reserve(numDistricts);
-        midPoints[i].reserve(2);
-        endPoints[i].reserve(2);
+        midPoints[i].reserve(numDistricts);
+        endPoints[i].reserve(numDistricts);
     }
     
-    // create 3 random points
+    // create numDistricts random points based on district span
     srand(time(0));
     
     for (int i = 0; i < numDistricts; i++){
         
         // x value
-        startPoints[i][0] = rand() % sceneWidth;
+        startPoints[i][0] = rand() % districtSpan;
         
         // y value
-        startPoints[i][1] = rand() % sceneHeight;
+        startPoints[i][1] = rand() % districtSpan;
+        
+        // this code ensures that neither the x nor the y values of this random point
+        // are too close to another existing random point. it will re-randomize if so.
+        if (!startPoints.empty()){
+            for (int j = 0; j < startPoints.size(); j++){
+                if (startPoints[i][0] != startPoints[j][0]){
+                    if (abs(startPoints[i][0] - startPoints[j][0]) < districtSpan/10){
+                        while(abs(startPoints[i][0] - startPoints[j][0]) < 100){
+                            startPoints[i][0] = rand() % districtSpan;
+                    }
+                }
+                    
+                if (startPoints[i][1] != startPoints[j][1]){
+                    if (abs(startPoints[i][1] - startPoints[j][1]) < districtSpan/10){
+                        while(abs(startPoints[i][1] - startPoints[j][1]) < 100){
+                            startPoints[i][1] = rand() % districtSpan;
+                        }
+                    }
+                }
+            }
+        }
+    }
+        
     }
     
-
     
-    for (int i = 0; i < numDistricts; i++){
-        
-        cout << startPoints[i][0] << "," << startPoints[i][1] << endl;
-        
-    }
+//    for (int i = 0; i < numDistricts; i++){
+//        cout << startPoints[i][0] << "," << startPoints[i][1] << endl;
+//    }
     
+    
+    // to save triangle center point
     int tCenterX = 0;
     int tCenterY = 0;
     
-    // calculate midpoints, ignoring pre-existing pairs
+    // calculate midpoints of all random initial points
     for (int i = 0; i < numDistricts; i++){
         
         for (int j = 0; j  < numDistricts; j++){
             
             if (i!=j){
-                
                 int x = (startPoints[i][0]+startPoints[j][0])/2;
                 int y = (startPoints[i][1]+startPoints[j][1])/2;
                 
                 vector<int> point { x,y };
-                
                 midPoints.push_back(point);
-                    cout << "midpoint for points " << i << " and " << j << " is " << x << ", " << y << endl;
             }
-            
         }
-        
     }
     
-    for (int i = 0; i < midPoints.size(); i++){
-        
-        cout << midPoints[i][0] << "," << midPoints[i][1] << endl;
-    }
+//    for (int i = 0; i < midPoints.size(); i++){
+//        cout << midPoints[i][0] << "," << midPoints[i][1] << endl;
+//    }
     
+    // remove unnecessary duplicate points
     sort(midPoints.begin(), midPoints.end());
     midPoints.erase(unique(midPoints.begin(), midPoints.end()), midPoints.end());
     
+    
+    // using these midpoints, get the center of the triangle
     for (int i = 0; i < midPoints.size(); i++){
-        
-        cout << midPoints[i][0] << "," << midPoints[i][1] << endl;
         
         tCenterX += midPoints[i][0];
         tCenterY += midPoints[i][1];
     }
     
-    // using these midpoints, get the center of the triangle
     tCenterX = tCenterX/numDistricts;
     tCenterY = tCenterY/numDistricts;
     
     cout << "midpoint for triangle is " << tCenterX << ", " << tCenterY << endl;
     
-    // for each point, get the slope and it's length based on distance from the edge of the buffer
+    
+    // for each point, get the slope and it's length based districtSpan and its position in relation to the triangle center
     for (int i = 1; i < numDistricts+1; i++){
         
         float slope = ((float)(tCenterY-midPoints[i][1]))/((float)(tCenterX-midPoints[i][0]));
         
         cout << "(" << tCenterX << "," << tCenterY << ") to (" << midPoints[i][0] << "," << midPoints[i][1] << ")" << endl;
-        cout << " slope is " << slope << endl;
         
-        int d = 300;
         
-        float k = d/(sqrt(1+pow(slope, 2.0)));
-        cout << k << endl;
+        // compute k for endpoint equation
+        float k = districtSpan/(sqrt(1+pow(slope, 2.0)));
         
+        // account for position of point relative in space when assigning end point
         float x,y;
         if (midPoints[i][0] < tCenterX){
             x = (float)tCenterX - k;
@@ -152,20 +165,16 @@ int main(int argc, const char * argv[]) {
         
     }
     
-    
+    // remove unecessary duplicates
     sort(endPoints.begin(), endPoints.end());
     endPoints.erase(unique(endPoints.begin(), endPoints.end()), endPoints.end());
     
+    // lines are just (tCenterX,tCenterY)->endPoints(i);
+    // to print to file, pipe cout to file name
     for (int i = 1; i<endPoints.size(); i++){
         
-        cout << "(" << endPoints[i][0] << ", " << endPoints[i][1] << ")" << endl;
+        cout << "(" << tCenterX << "," << tCenterY << "),(" << endPoints[i][0] << ", " << endPoints[i][1] << ")" << endl;
     }
-    
-    
-    
-    // lines are just (tCenterX,tCenterY)->endPoints(i);
-    
-    
     
     
     
