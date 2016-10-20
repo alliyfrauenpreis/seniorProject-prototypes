c6f472a8b0027e18e95a9fa8aab678935f475060
Frauenpreis <anf46@n1-140-194.dhcp.drexel.edu>
2016-10-20 18:33:53 -0400

    code cleanup

--- Districts/Assets/DistrictsGenerator.cs
+++ Districts/Assets/DistrictsGenerator.cs
@@ -3,36 +3,40 @@ using System.Collections;
 
 public class DistrictsGenerator : MonoBehaviour {
 
+	[SerializeField]
 	private float[,] districtEndPoints;
+
+	[SerializeField]
 	private float    cityCenterX, cityCenterY;
 
-	// Use this for initialization
+
+	/// <summary>
+	/// Update the text to face the camera if shown.
+	/// </summary>
 	void Start () {
-
-		generateDistrictPoints (3, 500);
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+		// generateDistrictPoints (3, 500);
 	}
 
+	/// <summary>
+	/// Generates city center and district edges. Edges of districts are defined as (cityCenterX,cityCenterY)->(districtEndPoints[i,0],districtEndPoints[i,1])
+	/// <param name="numDistricts">Number of districts desired -- currently supports 3.</param>
+	/// <param name="districtMaxSpan">District max span for one side from center point to edge.</param>
+	/// </summary>
 	void generateDistrictPoints(int numDistricts, int districtMaxSpan){
 
-		int[,] initialSeedPoints = new int[numDistricts,2];
-		int[,] seedMidPoints	 = new int[numDistricts,2];
-		districtEndPoints 		 = new float[numDistricts,2];
+		int[,] initialSeedPoints = new int	[numDistricts,2];
+		int[,] seedMidPoints	 = new int	[numDistricts,2];
+			   districtEndPoints = new float[numDistricts,2];
 
-		for (int i = 0; i < numDistricts; i++) {
-
+		// generate initial random seed points
+		for (int i = 0; i < numDistricts; ++i) {
 			initialSeedPoints[i,0] = Random.Range(1, districtMaxSpan);
 			initialSeedPoints[i,1] = Random.Range(1, districtMaxSpan);
 		}
 
-		// calculate midpoints of all initial seed points
-		for (int i = 0; i < numDistricts; i++){
-			for (int j = 0; j  < numDistricts; j++){
+		// calculate midpoints of all initial seeds
+		for (int i = 0; i < numDistricts; ++i){
+			for (int j = 0; j  < numDistricts; ++j){
 					int x = (initialSeedPoints[i,0]+initialSeedPoints[j,0])/2;
 					int y = (initialSeedPoints[i,1]+initialSeedPoints[j,1])/2;
 					seedMidPoints[j,0] = x;
@@ -40,13 +44,11 @@ public class DistrictsGenerator : MonoBehaviour {
 			}
 		}
 
-
-		// using these midpoints, get the center of the districts
+		// using these midpoints, get the center of the city
 		cityCenterX = 0;
 		cityCenterY = 0;
 
-		for (int i = 0; i < numDistricts; i++){
-
+		for (int i = 0; i < numDistricts; ++i){
 			cityCenterX += seedMidPoints[i,0];
 			cityCenterY += seedMidPoints[i,1];
 		}
@@ -54,30 +56,21 @@ public class DistrictsGenerator : MonoBehaviour {
 		cityCenterX = cityCenterX/numDistricts;
 		cityCenterY = cityCenterY/numDistricts;
 
-
 		// for each point, get the slope and it's length based districtSpan and its position relative to city center
-		for (int i = 0; i < numDistricts; i++){
+		for (int i = 0; i < numDistricts; ++i){
 
 			float slope = ((float)(cityCenterY-seedMidPoints[i,1]))/((float)(cityCenterX-seedMidPoints[i,0]));
-
-			// compute k for endpoint equation
 			float k = districtMaxSpan/(Mathf.Sqrt(1+Mathf.Pow(slope, 2.0f)));
 
 			// account for position of point relative in space when assigning end point
 			float currentEndpointX = 0.0f;
 			float currentEndpointY = 0.0f;
 
-			if (seedMidPoints[i,0] < cityCenterX){
-				currentEndpointX = (float)cityCenterX - k;
-			} else {
-				currentEndpointX = (float)cityCenterX + k;
-			}
-
-			if (seedMidPoints[i,1] < cityCenterY){
-				currentEndpointY = (float)cityCenterY - (k*slope);
-			} else {
-				currentEndpointY = (float)cityCenterY + (k*slope);
-			}
+			if (seedMidPoints[i,0] < cityCenterX)	currentEndpointX = (float)cityCenterX - k;
+			else 									currentEndpointX = (float)cityCenterX + k;
+		
+			if (seedMidPoints[i,1] < cityCenterY)	currentEndpointY = (float)cityCenterY - (k*slope);
+			else 									currentEndpointY = (float)cityCenterY + (k*slope);
 
 			districtEndPoints [i, 0] = currentEndpointX;
 			districtEndPoints [i, 1] = currentEndpointY;
